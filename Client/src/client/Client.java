12895cb45bc12b2699af9aeb9dd2ac5f562e3d62
aedemirsen <aedemirsen@gmail.com>
2017-04-12 21:57:20 +0300

    12.04.2017

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -11,24 +11,43 @@ package client;
  */
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class Client {
 
-    public static void main(String args[]) throws Exception {
-        BufferedReader inFromUser
-                = new BufferedReader(new InputStreamReader(System.in));
-        DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
-        String sentence = inFromUser.readLine();
-        sendData = sentence.getBytes();
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 1299);
-        clientSocket.send(sendPacket);
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        clientSocket.receive(receivePacket);
-        String modifiedSentence = new String(receivePacket.getData());
-        System.out.println("FROM SERVER:" + modifiedSentence);
-        clientSocket.close();
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("IP : ");
+        String ip = sc.nextLine();
+        System.out.print("Port : ");
+        int port = sc.nextInt();
+
+        try {
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
+
+            DatagramSocket clientSocket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName(ip);
+            System.out.print("Mesajınız: ");
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+            String line = bf.readLine();
+            sendData = line.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            clientSocket.send(sendPacket);
+            System.out.println("Mesaj Gönderildi...");
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            clientSocket.close();
+
+        } catch (SocketException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -9,27 +9,42 @@ package server;
  *
  * @author aedemirsen
  */
-import java.io.*;
+import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class Server {
 
-    public static void main(String args[]) throws Exception {
-        DatagramSocket serverSocket = new DatagramSocket(1299);
-        byte[] receiveData = new byte[1024];
-        byte[] sendData = new byte[1024];
-        while (true) {
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            serverSocket.receive(receivePacket);
-            String sentence = new String(receivePacket.getData());
-            System.out.println("RECEIVED: " + sentence);
-            InetAddress IPAddress = receivePacket.getAddress();
-            int port = receivePacket.getPort();
-            String capitalizedSentence = sentence.toUpperCase();
-            sendData = capitalizedSentence.getBytes();
-            DatagramPacket sendPacket
-                    = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-            serverSocket.send(sendPacket);
+    public static void main(String args[])  {
+        
+        Scanner sc = new Scanner(System.in);        
+        System.out.print("Port : ");
+        int port = sc.nextInt();
+        
+        try {
+            DatagramSocket serverSocket = new DatagramSocket(port);
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+            DatagramPacket receivePacket;
+            System.out.println("Server Çalışıyor...");
+            while (true) {
+                receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                String s = new String(receivePacket.getData());
+                System.out.println("Gelen Data: " + s);
+                InetAddress IPAddress = receivePacket.getAddress();
+                port = receivePacket.getPort();
+                sendData = s.getBytes();
+                DatagramPacket sendPacket
+                        = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                serverSocket.send(sendPacket);
+            }
+        } catch (SocketException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
